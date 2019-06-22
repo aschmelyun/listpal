b65014139b0a7df1971cb3f17e6d659ffc5c1294
Andrew Schmelyun <schmelyun@gmail.com>
2019-06-22 18:08:40 -0400

    Changes broadcasting scheme to https

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,14 +37,9 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => false,
                 'host' => '127.0.0.1',
                 'port' => 6001,
-                'scheme' => 'http',
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ]
+                'scheme' => 'https'
             ],
         ],
 
