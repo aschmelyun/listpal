e641e628d5d81dd3bb239f133525617ef8909528
Andrew Schmelyun <schmelyun@gmail.com>
2019-06-22 20:48:23 -0400

    Changes more echo settings for wss

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -31,6 +31,8 @@ window.Echo = new Echo({
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
     wsPort: 6001,
+    wssPort: 6001,
     disableStats: true,
-    encrypted: true
+    encrypted: true,
+    enabledTransports: ['ws', 'wss']
 });
