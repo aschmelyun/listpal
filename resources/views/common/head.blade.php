e0a2a352267a60549cfe3fdb35d04b8d8917710f
Andrew Schmelyun <schmelyun@gmail.com>
2019-06-27 23:52:28 -0400

    Replaces img with images in assets paths

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -8,23 +8,23 @@
     <link href="https://fonts.googleapis.com/css?family=Assistant:700|Roboto:400,500,700&display=swap" rel="stylesheet">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="mobile-web-app-capable" content="yes">
-    <link rel="apple-touch-icon" href="/assets/img/icons/icon_120x120.png">
-    <link rel="apple-touch-icon" sizes="57x57" href="/assets/img/icons/icon_57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/assets/img/icons/icon_60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/assets/img/icons/icon_72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/icons/icon_76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/assets/img/icons/icon_114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/assets/img/icons/icon_120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/assets/img/icons/icon_144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/assets/img/icons/icon_152x152.png">
-    <link rel="apple-touch-icon" sizes="167x167" href="/assets/img/icons/icon_167x167.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/icons/icon_180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="/assets/img/icons/icon_192x192.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/icons/icon_16x16.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/icons/icon_32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/assets/img/icons/icon_96x96.png">
+    <link rel="apple-touch-icon" href="/assets/images/icons/icon_120x120.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="/assets/images/icons/icon_57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/assets/images/icons/icon_60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/assets/images/icons/icon_72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/assets/images/icons/icon_76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/assets/images/icons/icon_114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/assets/images/icons/icon_120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/assets/images/icons/icon_144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/assets/images/icons/icon_152x152.png">
+    <link rel="apple-touch-icon" sizes="167x167" href="/assets/images/icons/icon_167x167.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/icons/icon_180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/assets/images/icons/icon_192x192.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/icons/icon_16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/icons/icon_32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/assets/images/icons/icon_96x96.png">
     <link rel="manifest" href="/manifest.json">
     <meta name="msapplication-TileColor" content="#424242">
-    <meta name="msapplication-TileImage" content="/assets/img/icons/icon_144x144.png">
+    <meta name="msapplication-TileImage" content="/assets/images/icons/icon_144x144.png">
     <meta name="theme-color" content="#424242">
 </head>
