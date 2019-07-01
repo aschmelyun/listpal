5fc10c925f4f555044fb4e2d088e72e3888ff027
Andrew Schmelyun <schmelyun@gmail.com>
2019-07-01 15:08:57 -0400

    Adds in cache-busting to webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/assets/js')
     .sass('resources/sass/app.scss', 'public/assets/css')
-    .copy('resources/images/', 'public/assets/images');
+    .copy('resources/images/', 'public/assets/images')
+    .version();
