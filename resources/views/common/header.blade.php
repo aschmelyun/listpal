f8236a1016999993df4fe0e7e51214aef818e3ee
Andrew Schmelyun <schmelyun@gmail.com>
2019-07-06 03:26:19 -0400

    Adds link to the list header logo, modifies autocomplete to off instead of false

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -1,4 +1,4 @@
 <header class="header">
-    <h1 class="header-title">ListPal</h1>
+    <a class="header-title" href="/">ListPal</a>
     <a href="#" class="add-button" :class="{'is-exit': showNewItem}" @click.prevent="showNewItem = !showNewItem"></a>
 </header>
